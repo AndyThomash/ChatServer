b932cc16adc89340f93eb462aec81c8aea5c3e48
deluxebwl <102295197+deluxebwl@users.noreply.github.com>
2022-05-11 12:39:23 +0200

    Das ist cool.

---
+++ NickVerwaltung.java
@@ -0,0 +1,31 @@
+import java.net.Socket;
+
+public class NickVerwaltung
+{
+ public NickVerwaltung() {
+
+ }
+
+
+    /**
+     * Methode zur Verarbeitung des eingegebenen Nicknames.
+     * @param name Name
+     * @param socket Socket
+     * @return String, socket
+     */
+ public boolean putNickname(String name, Socket socket) {
+     return false;
+ }
+
+
+    /**
+     * Methode zum Herausfinden des Nicknames.
+     * @param socket Socket
+     * @return Socket or Null
+     */
+ public String getNickname(Socket socket){
+     return null;
+ }
+
+
+}
